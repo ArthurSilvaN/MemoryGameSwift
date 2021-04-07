1c647a46bad958f9267cbb7d44ab514cee20c4d3
arthur.nunes <arthur.nunes@dtidigital.com.br>
2021-04-07 11:26:45 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by user on 07/04/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
