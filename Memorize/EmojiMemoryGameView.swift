430b61768c92e58f241b5cc5482652f49f477acd
arthur.nunes <arthur.nunes@dtidigital.com.br>
2021-04-15 14:47:06 -0300

    L4: Grid + enum + Optionals | Part I

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -11,16 +11,14 @@ struct EmojiMemoryGameView: View {
     @ObservedObject var viewModel: EmojiMemoryGame
     
     var body: some View {
-        HStack {
-            ForEach (viewModel.cards){ card in
+        Grid(viewModel.cards) { card in
                 CardView(card: card).onTapGesture {
                     self.viewModel.choose(card: card)
                 }
             }
-        }
-        .padding()
-        .foregroundColor(Color.orange)
-        .font(Font.largeTitle)
+                .padding()
+                .foregroundColor(Color.orange)
+                .font(Font.largeTitle)
     }
 }
 
